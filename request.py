4d1e9f831c6560cbf8ad0590207e7282f56bdff0
Bryanna Davison <bryanna@Motoko.local>
2016-06-27 13:52:24 -0400

    Update

--- request.py
+++ request.py
@@ -1,20 +1,33 @@
 class request:
     # This is the initializer for the request class
     def __init__(self, sourceNode, destNode, volume, timeStamp,timeTrack,lastReqFlag):
-        self.sourceNode = sourceNode
-        self.destNode = destNode
-        self.volume = volume
-        self.timeStamp = timeStamp
-        self.timeTrack = timeTrack
-        self.lastReqFlag = lastReqFlag
-        self.scheduledFlag= 0
-        self.transmittedFlag = 0  
+        self.sourceNode     = sourceNode
+        self.destNode       = destNode
+        self.volume         = volume
+        self.timeStamp      = timeStamp
+        self.timeTrack      = timeStamp
+        self.lastReqFlag    = lastReqFlag
+        self.scheduled      = False
+        self.transmitted    = False
         
     def __del__(self):
         
-    def reqProcessing():
-        
+    def reqProcessing(t):
+        if ~scheduled:
+            schedule(t)
+        else if ~transmitted:
+            transmit()
+        else if timeTrack == t:
+
+
     def schedule():
+        if sourceNode < destNode:
+            if nodestat[sourceNode:(destNode +1)] = False
+                nodestat[sourceNode:(destNode +1)] = [1] * ((destNode +1)-sourceNode)
+                schedule = True
+                timeTrack += timeSchedule + time
+
+
     
     def transmit(): 
     pass
---
+++ simulation.py
@@ -0,0 +1,9 @@
+activereq = []
+nodestat = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+isover = False
+OCC = 40 #GHz
+ECC = 2 #GHz
+packetSize = 1 #bits
+
+
+while ~isover:
