c7ea90255f052b333c81af9cf5420ff48ae56cf5
aberlanga <alexcberlanga@gmail.com>
2016-06-27 14:08:02 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,3 +1,12 @@
+#Global time constants to account for certain processes.
+EccToOcc = OCC/ECC
+tBuffer= 1 *EccToOcc
+tMUX= 1*EccToOcc
+tSequence= 1*EccToOcc
+tSchedule= 1*EccToOcc
+tOpenChannels= 1*EccToOcc
+tCloseChannels= 1*EccToOcc
+
 activereq = []
 nodestat = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 isover = False
