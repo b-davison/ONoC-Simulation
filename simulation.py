d1d7064ad5d551c52d0395f54c86e865c6c8ed5f
aberlanga <alexcberlanga@gmail.com>
2016-06-27 14:09:42 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,3 +1,10 @@
+activereq = []
+nodestat = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+isover = False
+OCC = 40 #GHz
+ECC = 2 #GHz
+packetSize = 1 #bits
+
 #Global time constants to account for certain processes.
 EccToOcc = OCC/ECC
 tBuffer= 1 *EccToOcc
@@ -6,13 +13,6 @@ tSequence= 1*EccToOcc
 tSchedule= 1*EccToOcc
 tOpenChannels= 1*EccToOcc
 tCloseChannels= 1*EccToOcc
-
-activereq = []
-nodestat = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-isover = False
-OCC = 40 #GHz
-ECC = 2 #GHz
-packetSize = 1 #bits
 
 
 while ~isover:
