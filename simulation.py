9fa367b191325626857da177093424b48ecc8ed5
aberlanga <alexcberlanga@gmail.com>
2016-06-29 15:56:10 -0400

    Add files via upload
    This is my added work to the simulation.py. In this change I have created an active request list and added to the iterating loop to check each clock cycle for a request and add it to the active request list.

--- simulation.py
+++ simulation.py
@@ -1,31 +1,59 @@
-#Global Constants for system parameters
-OCC 		    = 40 #GHz
-ECC             = 2 #GHz
-packetSize      = 1 #bits
-nodeCount       = 16 #number of nodes
-weighted_cutoff = 9	#number of nodes preoccupied 
+import request
 
+#Converts a log file of XY coordinates to node numbers and returns a reduced list of lists that contains the log information in its elements.
+#Ex.     S|D |#|V|tStamp
+#       [1,15,1,4 , 0  ],
+#       [2,3,2,3  , 30 ],...
+#       
+def convXYtoNode(logFile):
+    benchmark = []
+
+    with open(logFile,"r") as bmread:
+        for line in bmread:
+            benchmarkLine = []
+            benchmarkItems = line.split()
+            for i in benchmarkItems:
+                benchmarkLine.append(int(i))
+            benchmark.append(benchmarkLine)
+
+    newBenchmark = []
+    for row in benchmark:
+        if (row[0] != 0) & (row[2] != 0):
+            newRow = [(row[0] - 1) * 8 + row[1], (row[2] - 1) * 8 + row[3]]
+            newRow.extend(row[4:])
+            newBenchmark.append(newRow)
+    return newBenchmark
+
+
+
+
+
+
+activeReq = []
+nodestat = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+isover = False
+OCC = 40 #GHz
+ECC = 2 #GHz
+packetSize = 1 #bits
+logFile = 'test_flow_barnes.log' # the log file that will be tested
 
 #Global time constants to account for certain processes.
-EccToOcc       = OCC/ECC
-tBuffer        = 1 *EccToOcc
-tMUX           = 1 *EccToOcc
-tSequence      = 1 *EccToOcc
-tSchedule      = 1 *EccToOcc
-tOpenChannels  = 1 *EccToOcc
-tCloseChannels = 1 *EccToOcc
+EccToOcc = OCC/ECC
+tBuffer= 1 *EccToOcc
+tMUX= 1*EccToOcc
+tSequence= 1*EccToOcc
+tSchedule= 1*EccToOcc
+tOpenChannels= 1*EccToOcc
+tCloseChannels= 1*EccToOcc
 
-
-#Global Variable to act as system processes
-activereq = []
-nodestat  = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-isover 	  = False
-
-
-# tracks time in simulation, uses OCC as a time step
-t = 0
-
-
-
+nodeBenchmarkList = convXYtoNode(logFile)
+endFlag=False
+t=0
+reqCount=0
 
 while ~isover:
+    while t==nodeBenchmarkList[reqCount][4]:
+        if reqCount+1 == len(nodeBenchmarkList):
+            endFlag=True
+        activeReq.append(request(nodeBenchmarkList[reqCount][0],nodeBenchmarkList[reqCount][1],nodeBenchmarkList[reqCount][3],nodeBenchmarkList[reqCount][4],nodeBenchmarkList[reqCount][4],endFlag))
+    t += 1
