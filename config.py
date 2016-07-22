f70a8c2d63737d1739f52225fdf744c78d0104a5
Bryanna Davison <bryanna@Motoko.local>
2016-07-21 22:48:07 -0400

    added nn.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import os, sys
 
 # Pre: Requires correct directory of benchmarks in variable path
 # Post: Creates list of benchmarks
-path = 'C:\\Users\\NinetyBear\\Documents\\Visual Studio 2015\\Projects\\ONoC Simulator\\PythonApplication1\\Benchmark Data\\16-core'
-dirs = os.listdir(path)
-benchmarks = []
-benchmarksOnly = []
+# path = 'C:\\Users\\NinetyBear\\Documents\\Visual Studio 2015\\Projects\\ONoC Simulator\\PythonApplication1\\Benchmark Data\\16-core'
+# dirs = os.listdir(path)
+# benchmarks = []
+# benchmarksOnly = []
 
 # Appends all benchmarks in directory into one list
 
-for file in dirs:
-    benchmarks.append(file)
-    benchmarksOnly.append(file)
-for i in range(0,len(benchmarks)):
-    benchmarks[i] = path + '\\' + benchmarks[i]
+# for file in dirs:
+#     benchmarks.append(file)
+#     benchmarksOnly.append(file)
+# for i in range(0,len(benchmarks)):
+#     benchmarks[i] = path + '\\' + benchmarks[i]
 activeReq = []
 nodestate = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 isover = False
@@ -25,7 +25,7 @@ nodeCount = 16
 
 weighted_cutoff = 9 # max nodes allowed to be bypassed on furthest path
 
-logFile = 'test_flow_barnes.log' # the log file that will be tested
+logFile = 'flow_barnes.log' # the log file that will be tested
 weighted_cutoff = 9 # max nodes allowedd to be bypassed on furthest path
 
 
---
+++ nn.py
@@ -0,0 +1,108 @@
+
+import config
+import time
+import numpy as np
+
+# borrowed code    
+def convXYtoNode(logFile):
+    benchmark = []
+
+    with open(logFile,"r") as bmread:
+        for line in bmread:
+            benchmarkLine = []
+            benchmarkItems = line.split()
+            for i in benchmarkItems:
+                benchmarkLine.append(int(i))
+            benchmark.append(benchmarkLine)
+
+    newBenchmark = []
+    for row in benchmark:
+        if (row[0] != 0) & (row[2] != 0):
+            newRow = [(row[0] - 1) * 8 + row[1], (row[2] - 1) * 8 + row[3]]
+            newRow.extend(row[4:])
+            newBenchmark.append(newRow)
+    return newBenchmark
+def keyMapping(key):
+    for row in benchmark:
+        row[1] = key[row[1]]
+        row[2] = key[row[2]]
+
+startTime = time.time()
+nodeBenchmarkList = convXYtoNode(config.logFile)
+# borrowed code
+
+def rank():
+    n = 0
+    for row in trafficto:
+        for i in range(0,n):
+            if ~(row[4] == 0):
+                trafcounts.append(row[i])
+                trafpositions.append((n,i,row[i]))
+        n += 1
+
+def getkey(item):
+    return item[2]
+
+
+def NN(A, start):
+    """Nearest neighbor algorithm.
+    A is an NxN array indicating distance between N locations
+    start is the index of the starting location
+    Returns the path and cost of the found solution
+    """
+    path = [start]
+    cost = 0
+    N = A.shape[0]
+    mask = np.ones(N, dtype=bool)  # boolean values indicating which 
+                                   # locations have not been visited
+    mask[start] = False
+
+    for i in range(N-1):
+        last = path[-1]
+        next_ind = np.argmin(A[last][mask]) # find minimum of remaining locations
+        next_loc = np.arange(N)[mask][next_ind] # convert to original location
+        path.append(next_loc)
+        mask[next_loc] = False
+
+    return path
+
+trafficto = np.zeros((16,16))
+for row in trafficto:
+	print row 
+trafcounts = []
+trafpositions = []
+key = []
+
+print '\n'
+
+for row in nodeBenchmarkList:
+    print row[4]
+    trafficto[row[0]][row[1]] += row[4]
+    trafficto[row[1]][row[0]] += row[4]
+
+for row in trafficto:
+	print row 
+
+
+rank()
+ntraffic = sorted(trafpositions, key=getkey)
+
+
+key = NN(trafficto, ntraffic[0][1])
+
+print "This is the key"
+print key
+
+
+
+# print ntraffic
+
+# for row in ntraffic:
+#     s = row[1]
+#     d = row[2]
+#     #check key
+#     if s in key:
+#         if d in key:
+
+
+
